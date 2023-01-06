089f4578de3cc8777dde72689dfd6a872403bf37
CatThingy <andyc.andychen@gmail.com>
2023-01-06 13:41:21 -0800

    feat: main menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+use iyes_loopless::prelude::*;
+
+mod main_menu;
+
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+pub enum GameState {
+    MainMenu,
+    InGame,
+}
+
+#[derive(Component)]
+pub struct MainCamera;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_loopless_state(GameState::MainMenu)
+        .add_plugin(main_menu::Plugin)
+        .add_startup_system(init);
+
+    app.run();
+}
+
+fn init(mut cmd: Commands) {
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 0.25;
+    cmd.spawn((camera, MainCamera));
 }
